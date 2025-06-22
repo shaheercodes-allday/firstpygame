21258bcb68a09cf3cb235abc1c381fe664723b18
Shaheer Shaikh <shaheercodes.allday@gmail.com>
2025-06-22 11:05:40 +0530

    Add player walk animation . A new enemy (Flame)

--- main.py
+++ main.py
@@ -14,13 +14,23 @@ def display_score():
 def update_obstacles(ol):
     if ol:
         for o in ol:
-            screen.blit(centipede_surface, o)
-            o.x -= 5
+            if o.bottom == 250:
+                screen.blit(centipede_surface, o)
+                o.x -= 6
+            else:
+                screen.blit(flame_surface, o)
+                o.x -= 7
     
         ol = [x for x in ol if x.x > -100]
         return ol
     else:
         return []
+    
+def rogue_walk_animate():
+    global rogue_surface, rogue_walk_idx
+    rogue_walk_idx += 0.2
+    if rogue_walk_idx >= len(rogue_walk): rogue_walk_idx = 0
+    rogue_surface = rogue_walk[int(rogue_walk_idx)]
 
 # Constants
 # Frame Per Second to ensure the game won't run too fast
@@ -43,10 +53,27 @@ pygame.display.set_caption("Running Rogue")
 sky_surface = pygame.image.load("graphics/purple-sky.png")
 ground_surface = pygame.image.load("graphics/ground.png")
 
-rogue_surface = pygame.image.load("characters/Rogue/Walk/walk1.png")
+rogue_walk1 = pygame.image.load("characters/Rogue/Walk/walk1.png")
+rogue_walk2 = pygame.image.load("characters/Rogue/Walk/walk2.png")
+# rogue_walk3 = pygame.image.load("characters/Rogue/Walk/walk3.png")
+# rogue_walk4 = pygame.image.load("characters/Rogue/Walk/walk4.png")
+rogue_walk5 = pygame.image.load("characters/Rogue/Walk/walk5.png")
+rogue_walk6 = pygame.image.load("characters/Rogue/Walk/walk6.png")
+
+rogue_walk = [
+    rogue_walk1,
+    rogue_walk2,
+    # rogue_walk3,
+    # rogue_walk4,
+    rogue_walk5,
+    rogue_walk6
+]
+
+rogue_walk_idx = 0
 # Create rectangle and bind it to the rogue_surface 
 # to control positioning more easily
-rogue_rect = rogue_surface.get_rect(bottomleft = (30, 250))
+rogue_surface = rogue_walk[rogue_walk_idx]
+rogue_rect = rogue_walk[rogue_walk_idx].get_rect(bottomleft = (30, 250))
 
 rogue_stand = pygame.image.load("characters/Rogue/rogue.png")
 rogue_stand = pygame.transform.rotozoom(rogue_stand, 0, 2)
@@ -55,20 +82,19 @@ rogue_stand_rect = rogue_stand.get_rect(center = (283, 175))
 player_gravity = 0
 
 centipede_surface = pygame.image.load("characters/Centipede.png")
-centipede_rect = centipede_surface.get_rect(bottomright = (420, 250))
+flame_surface = pygame.image.load("characters/Flame.png")
 
 game_title = font.render("Running Rogue", False, "white")
 game_title_rect = game_title.get_rect(center = (283, 50))
-replay_hint = font.render("Press space to play agian!", False, "white")
+replay_hint = font.render("Press R to Play!", False, "white")
 replay_hint_rect = replay_hint.get_rect(center = (283, 290))
 
 obstacle_timer = pygame.USEREVENT + 1
-pygame.time.set_timer(obstacle_timer, 1600)
+pygame.time.set_timer(obstacle_timer, 1500)
 
 obstacle_list = []
 
 game_active = False
-centipede_speed = 4
 
 while True:
     # The event loop
@@ -80,11 +106,16 @@ while True:
         if game_active:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE and rogue_rect.bottom >= 250:
-                    player_gravity = -20
+                    player_gravity = -18
             if event.type == obstacle_timer:
-                obstacle_list.append(centipede_surface.get_rect(bottomright = (randint(600, 1000), 250)))
+                obstacle_types = [
+                    centipede_surface.get_rect(bottomright = (randint(600, 1000), 250)),
+                    flame_surface.get_rect(bottomright = (randint(600, 1000), randint(150, 240)))
+                ]
+
+                obstacle_list.append(obstacle_types[randint(0, 1)])
         else:
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_r:
                 obstacle_list = []
                 game_active = True
                 START_TIME = pygame.time.get_ticks()
@@ -92,9 +123,13 @@ while True:
         # Draw images
     if game_active:
         screen.blit(sky_surface, (0, 0))
+        # screen.fill("black")
         score = display_score()
         screen.blit(ground_surface, (0, 250))
         # Rogue suraface has the position coordinates of its rectangle
+
+        rogue_walk_animate()        
+
         screen.blit(rogue_surface, rogue_rect)
 
         # Gravity on player
